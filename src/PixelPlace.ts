e75784f0c4fe8db294163be4bc03b16ede207bae
Symmettry <109181931+Symmettry@users.noreply.github.com>
2024-09-17 23:17:12 +0200

    add sample headers for CF Clearance

--- src/PixelPlace.ts
+++ src/PixelPlace.ts
@@ -37,6 +37,38 @@ class PixelPlace {
     }
 
     /**
+     * Sets the headers of the bot to a pre-made one, but changed based on your cookies.
+     * @param cookie 
+     */
+    setCFClearance(cookie: string): PixelPlace {
+        return this.setHeaders((type) => {
+            const h: {[key: string]: string} = {
+                "Accept": "",
+                "Accept-Encoding": "gzip, deflate, br, zstd",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Cache-Control": "",
+                "Connection": "",
+                "Cookie": `cf_clearance=${cookie};`,
+                "Priority": "u=1, i",
+                "Referer": "https://pixelplace.io/",
+                "Sec-Ch-Ua": "\"Brave\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"",
+                "Sec-Ch-Ua-Mobile": "?0",
+                "Sec-Ch-Ua-Model": "\"\"",
+                "Sec-Ch-Ua-Platform": "\"Windows\"",
+                "Sec-Ch-Ua-Platform-Version": "\"10.0.0\"",
+                "Sec-Fetch-Dest": "empty",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Site": "same-origin",
+                "Sec-Gpc": "1",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+                "X-Requested-With": "XMLHttpRequest"
+            };
+            switch(type){case"socket":h["Cache-Control"]="no-cache";h["Connection"]="Upgrade";break;case"get-painting":h.Accept="application/json, text/javascript, */*; q=0.01";break;case "canvas-image":h.Accept="image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8";h["Cache-Control"]="no cache"}Object.keys(h).forEach(k =>{if(h[k].trim()==""){delete h[k]}});
+            return h;
+        });
+    }
+
+    /**
      * Initiates all the bots
      * @returns A promise that resolves upon the bots initiating fully.
      */
